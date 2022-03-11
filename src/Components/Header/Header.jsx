5b1524aa25d5440ce857cab0a25803204cafade5
Luis Salcedo Salas <rideluis@gmail.com>
2022-03-10 23:39:34 -0500

    feat: create header with scss

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import '../../App.scss'
+import './header.scss'
 
 export const Header = () => {
     return (
-      <div>
-        <li className="active">
-                  <Link to="/Login"> Login </Link>
-                  <Link to="/Signup"> Registro </Link>
-        </li>
+      <div className="header">
+        <Link to="/">
+          <img className="header__logo" src="pending_media_change_by_pacho" alt="Quick Call Logo" />
+        </Link>
+        <div className="header__search">
+          <input className="search__bar" type="text" name="search" id="search" placeholder="¿Qué necesitas?" />
+          <button>
+            <img className="search__icon" src="pending_media_change_by_pacho" alt="Search icon" />
+          </button>
+        </div>
+        <div className="header__menu">
+          <button className="menu__icon">
+            <img src="pending_media_change_by_pacho" alt="hamburguer-icon" />
+          </button>
+          <div className="menu__link-list">
+            <Link to="/Search" className="list__link">Servicios</Link>
+            <Link to="/Signup" className="list__link">Registro</Link>
+            <Link to="/Login" className="list__link">Iniciar Sesión</Link>
+          </div>
+        </div>
       </div>
     );
-  }
+}
