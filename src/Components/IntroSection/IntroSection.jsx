9ae45da003bf63636ceb94a7b356260e279a73c3
Luis Salcedo Salas <rideluis@gmail.com>
2022-03-14 15:58:33 -0500

    feat: complete landing page main section

--- src/Components/IntroSection/IntroSection.jsx
+++ src/Components/IntroSection/IntroSection.jsx
@@ -1,11 +1,36 @@
+import './IntroSection.scss'
+
 function IntroSection() {
     return (
         <div className="intro-section">
             <div className="intro-section__description">
-                <h1 className="intro-section__title">Necesitas Servicios?</h1>
-
+                <h1 className="intro-description__title">Necesitas Servicios?</h1>
+                <div className="intro-description__item">
+                    <img src="icons/magnifyng-glass-icon.svg" alt="Magnifying Glass" className="intro-item__img" />
+                    <p className="intro-item__text">
+                        Encuentra al profesional usando nuestro buscador o utilizando la función de filtro!
+                    </p>
+                </div>
+                <div className="intro-description__item">
+                    <img src="icons/chat-icon.svg" alt="Chat" className="intro-item__img" />
+                    <p className="intro-item__text">
+                        Contacta al profesional, rápido y fácil! por medio de nuestro chat
+                    </p>
+                </div>
+                <div className="intro-description__item">
+                    <img src="icons/wallet-icon-colored.svg" alt="Wallet" className="intro-item__img" />
+                    <p className="intro-item__text">
+                        Realiza tu pago en cualquiera de los diversos métodos que ofrecemos!
+                    </p>
+                </div>
+                <div className="intro-description__item">
+                    <img src="icons/worker-icon.svg" alt="Worker" className="intro-item__img" />
+                    <p className="intro-item__text">
+                        Recibe nuestros servicios de calidad!
+                    </p>
+                </div>
             </div>
-
+            <img src="img/bg-home.png" alt="background" className="intro-section__bg" />
         </div>
     )
 }
