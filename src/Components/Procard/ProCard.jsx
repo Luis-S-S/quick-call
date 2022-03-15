a899f494b86077745f6786b7a4abed1cd6a64c06
franciscocruz29 <franciscohuertas25@gmail.com>
2022-03-15 15:55:35 -0500

    Changed files according to Eslint

--- src/Components/Procard/ProCard.jsx
+++ src/Components/Procard/ProCard.jsx
@@ -1,7 +1,6 @@
-import React from "react";
 import './ProCard.scss';
 
-export const ProCard = () => {
+function ProCard() {
   return (
     <div className="procard">
       <div className="card">
@@ -10,16 +9,27 @@ export const ProCard = () => {
         </div>
         <div className="details">
           <div className="center">
-            <h1>Pepito Perez<br/><span>Constructor</span></h1>
+            <h1>
+              Pepito Perez
+              <br />
+              <span>Constructor</span>
+            </h1>
             <p>Lorem ipsum is simple dummy text on the printing and typesetting industry.</p>
             <ul>
-              <li><a href="#"><i className="fa fa-commenting-o" aria-hidden="true"></i></a></li>
-              <li><a href="#"><i className="fa fa-user" aria-hidden="true"></i></a></li>
-              <li><a href="#"><i className="fa fa-star" aria-hidden="true"></i><i className="fa fa-star-half-o" aria-hidden="true"></i></a></li>
+              <li><a href="#"><i className="fa fa-commenting-o" aria-hidden="true" /></a></li>
+              <li><a href="#"><i className="fa fa-user" aria-hidden="true" /></a></li>
+              <li>
+                <a href="#">
+                  <i className="fa fa-star" aria-hidden="true" />
+                  <i className="fa fa-star-half-o" aria-hidden="true" />
+                </a>
+              </li>
             </ul>
           </div>
         </div>
       </div>
     </div>
   );
-};
+}
+
+export default ProCard;
