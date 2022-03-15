4338f7b2a34eeb2f0611456447cd49f6ffc0abfb
Luis Salcedo Salas <rideluis@gmail.com>
2022-03-15 11:29:45 -0500

    feat: edit eslintrc.json .editorconfig package.json .vscode

--- src/components/Navbar/NavigationBar.jsx
+++ src/components/Navbar/NavigationBar.jsx
@@ -1,23 +1,22 @@
 import './NavigationBar.scss';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <nav>
         <input type="checkbox" id="check" />
-        <label for="check" className='checkbtn'>&#9776;</label>
-        <Link to='/'><img className='logo' src='logo/quick-call-logo--colored.svg' alt="logo"/></Link>
-        <input type="" placeholder="   ¿Qué necesitas?..." id='search'/>
+        <label htmlFor="check" className="checkbtn">&#9776;</label>
+        <Link to="/"><img className="logo" src="logo/quick-call-logo--colored.svg" alt="logo" /></Link>
+        <input type="" placeholder="   ¿Qué necesitas?..." id="search" />
         <ul>
-          <li><Link to='/'>Servicios</Link></li>
-          <li><Link to='/Signup'>Registrarse</Link></li>
-          <li><Link to='/Login'>Iniciar Sesión</Link></li>
+          <li><Link to="/">Servicios</Link></li>
+          <li><Link to="/Signup">Registrarse</Link></li>
+          <li><Link to="/Login">Iniciar Sesión</Link></li>
         </ul>
       </nav>
     </div>
-  )
+  );
 }
 
-export default NavBar
-
+export default NavBar;
