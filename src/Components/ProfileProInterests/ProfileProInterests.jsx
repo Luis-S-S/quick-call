c4af3cd1711351af2f36a04da4dd36a3da6db48a
franciscocruz29 <franciscohuertas25@gmail.com>
2022-03-16 15:43:43 -0500

    created ProfileProInterest component

--- src/Components/ProfileProInterests/ProfileProInterests.jsx
+++ src/Components/ProfileProInterests/ProfileProInterests.jsx
@@ -1,11 +1,19 @@
 import './ProfileProInterests.scss';
+import Carousel from 'react-elastic-carousel';
+import Procard from '../Procard/ProCard';
 
 function ProfileProInterest() {
   return (
-    <div>
-      <h1>Quizas te interese</h1>
+    <div className="interest">
+      <h1>Quizas te interese ...</h1>
+      <Carousel itemsToShow={3}>
+        <Procard />
+        <Procard />
+        <Procard />
+        <Procard />
+        <Procard />
+      </Carousel>
     </div>
-
   );
 }
 
