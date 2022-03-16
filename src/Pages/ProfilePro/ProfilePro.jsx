541ce46266b97a7d9fc23842cb969c8883115e3b
franciscocruz29 <franciscohuertas25@gmail.com>
2022-03-16 09:37:00 -0500

    Created ProfileProDescription component

--- src/Pages/ProfilePro/ProfilePro.jsx
+++ src/Pages/ProfilePro/ProfilePro.jsx
@@ -1,10 +1,12 @@
 import NavigationBar from '../../Components/Navbar/NavigationBar';
+import ProfileProDescription from '../../Components/ProfileProDescription/ProfileProDescription';
 import Footer from '../../Components/Footer/Footer';
 
 function ProfilePro() {
   return (
     <>
       <NavigationBar />
+      <ProfileProDescription />
       <Footer />
     </>
   );
