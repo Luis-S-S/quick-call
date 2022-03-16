9d8698d96483dc3db301647059bc5cfcb2025a16
franciscocruz29 <franciscohuertas25@gmail.com>
2022-03-16 09:44:31 -0500

    Fixed ProfileProDescription component

--- src/Components/ProfileProDescription/ProfileProDescription.jsx
+++ src/Components/ProfileProDescription/ProfileProDescription.jsx
@@ -3,40 +3,43 @@ import ButtonSquare from '../ButtonSquare/ButtonSquare';
 
 function ProfileProDescription() {
   return (
-    <div className="row">
-      <div className="column">
-        <img className="photo" src="./images/img/constructor.jpeg" alt="constructor" width={500} height={500} />
-      </div>
-      <div className="column">
-        <div className="description">
-          <h1>Pepito Perez</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel nisi nisi.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel nisi nisi.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel nisi nisi.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel nisi nisi.</p>
+    <div className="profileDescription">
+      <div className="row">
+        <div className="column">
+          <img className="photo" src="./images/img/constructor.jpeg" alt="constructor" width={500} height={500} />
         </div>
-        <div className="categories">
-          <h1>Mis especialidades</h1>
-          <div className="categoriesList">
-            <h4>Categoria</h4>
-            <h4>Categoria</h4>
-            <h4>Categoria</h4>
-            <h4>Categoria</h4>
-            <h4>Categoria</h4>
-            <h4>Categoria</h4>
+        <div className="column">
+          <div className="description">
+            <h1>Pepito Perez</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel nisi nisi.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel nisi nisi.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel nisi nisi.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel nisi nisi.</p>
           </div>
-        </div>
-        <div className="calification">
-          <span className="fa fa-star" />
-          <span className="fa fa-star" />
-          <span className="fa fa-star" />
-          <span className="fa fa-star" />
-          <span className="fa fa-star-half-o" />
-          <br />
-          <ButtonSquare text="Iniciar Chat" />
+          <div className="categories">
+            <h1>Mis especialidades</h1>
+            <div className="categoriesList">
+              <h4>Categoria</h4>
+              <h4>Categoria</h4>
+              <h4>Categoria</h4>
+              <h4>Categoria</h4>
+              <h4>Categoria</h4>
+              <h4>Categoria</h4>
+            </div>
+          </div>
+          <div className="calification">
+            <span className="fa fa-star" />
+            <span className="fa fa-star" />
+            <span className="fa fa-star" />
+            <span className="fa fa-star" />
+            <span className="fa fa-star-half-o" />
+            <br />
+            <ButtonSquare text="Iniciar Chat" />
+          </div>
         </div>
       </div>
     </div>
+
   );
 }
 
