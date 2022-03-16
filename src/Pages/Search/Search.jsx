b50aa0c7e94092ca67090e9f3535ae77c76202a8
franciscocruz29 <franciscohuertas25@gmail.com>
2022-03-16 17:44:07 -0500

    Added json procard

--- src/Pages/Search/Search.jsx
+++ src/Pages/Search/Search.jsx
@@ -1,12 +1,19 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import ProCard from '../../Components/Procard/ProCard';
+import { getAllPro } from '../../Services/pro';
 
 function Search() {
+  const [pro, setPro] = useState([]);
+
+  useEffect(() => {
+    getAllPro().then((data) => setPro(data));
+  }, []);
+
   return (
     <div>
-
       <Link to="/"> Ir a Home</Link>
-      <ProCard />
+      {pro.map((item) => <ProCard key={item.id} details={item} />)}
     </div>
 
   );
