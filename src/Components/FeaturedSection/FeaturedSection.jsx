73034a5f6752869b264e01db1f190ad7a434282d
Luis Salcedo Salas <rideluis@gmail.com>
2022-03-16 22:50:37 -0500

    feat: complete landing page, feat section is still local

--- src/Components/FeaturedSection/FeaturedSection.jsx
+++ src/Components/FeaturedSection/FeaturedSection.jsx
@@ -1,24 +1,36 @@
 import './FeaturedSection.scss';
+import Carousel from 'react-elastic-carousel';
 import ButtonSquare from '../ButtonSquare/ButtonSquare';
+import ProCard from '../Procard/ProCard';
 
 function FeaturedSection() {
   return (
     <div className="home__mid-section">
-      <div>
-        <h2>Nuestros profesionales mejor calificados</h2>
+      <div className="mid-section__left">
+        <h2 className="mid-section__title">Nuestros profesionales mejor calificados</h2>
+        <Carousel>
+          <ProCard />
+          <ProCard />
+          <ProCard />
+          <ProCard />
+        </Carousel>
       </div>
-      <div>
-        <h2>Que dicen nuestros clientes</h2>
+      <div className="mid-section__right">
+        <h2 className="mid-section__title">Que dicen nuestros clientes</h2>
         <ButtonSquare color="white" link="/">
+          <img className="mid-section__icon" src="images/icons/brush-icon-white.svg" alt="" />
           Categoría
         </ButtonSquare>
         <ButtonSquare color="white" link="/">
+          <img className="mid-section__icon" src="images/icons/tools-icon-white.svg" alt="" />
           Categoría
         </ButtonSquare>
         <ButtonSquare color="white" link="/">
+          <img className="mid-section__icon" src="images/icons/brush-icon-white.svg" alt="" />
           Categoría
         </ButtonSquare>
         <ButtonSquare color="white" link="/">
+          <img className="mid-section__icon" src="images/icons/tools-icon-white.svg" alt="" />
           Categoría
         </ButtonSquare>
       </div>
