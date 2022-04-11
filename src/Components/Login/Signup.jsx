969957d5c453d3d00c7b3a0db41c3651bc3a40c7
jesus osorio <jesdaos@hotmail.com>
2022-04-11 11:51:23 -0500

    Refactor login.scss

--- src/Components/Login/Signup.jsx
+++ src/Components/Login/Signup.jsx
@@ -7,28 +7,28 @@ export default function Signup() {
     <div className="login">
       <div className="container">
         <img className="logo" src="images/logo/quick-call-logo.svg" alt="" />
-      <div className="texto">
-        <span className="titulo_register">Registrarse</span>
-        <span className="texto_register">Qué tipo de usuario quieres registrar</span>
-      </div>
-      <form className="formulario">
-        <div className="boton11">
-          <ButtonRound link="/signup_client">Cliente</ButtonRound>
-          <ButtonRound link="/signup_professional">Profesional</ButtonRound>
+        <div className="texto">
+          <span className="titulo_register">Registrarse</span>
+          <span className="texto_register">Qué tipo de usuario quieres registrar</span>
         </div>
-        <div className="redes_sociales">
-          <img src="images/icons/whatsapp-logo.svg" alt="whatsapp" />
-          <img src="images/icons/facebook-icon.svg" alt="facebook" />
-          <img src="images/icons/twitter-icon.svg" alt="twitter" />
-          <img src="images/icons/linkedin-logo.svg" alt="linkedin" />
-        </div>
-        <div className="footer2">
-          <span className="footer11">
-            ¿Ya tienes una cuenta?.
-            <Link to="/login">Ingresa aqui</Link>
-          </span>
-        </div>
-      </form>
+        <form className="formulario">
+          <div className="boton11">
+            <ButtonRound link="/signup_client">Cliente</ButtonRound>
+            <ButtonRound link="/signup_professional">Profesional</ButtonRound>
+          </div>
+          <div className="redes_sociales">
+            <img src="images/icons/whatsapp-logo.svg" alt="whatsapp" />
+            <img src="images/icons/facebook-icon.svg" alt="facebook" />
+            <img src="images/icons/twitter-icon.svg" alt="twitter" />
+            <img src="images/icons/linkedin-logo.svg" alt="linkedin" />
+          </div>
+          <div className="footer2">
+            <span className="footer11">
+              ¿Ya tienes una cuenta?.
+              <Link to="/login">Ingresa aqui</Link>
+            </span>
+          </div>
+        </form>
       </div>
     </div>
   );
