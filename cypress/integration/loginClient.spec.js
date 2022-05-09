09e83a3d970b464e77c82b12d810ef2d9e0a0091
Luis Salcedo Salas <rideluis@gmail.com>
2022-05-09 17:41:32 -0500

    feat: dummy test

--- cypress/integration/loginClient.spec.js
+++ cypress/integration/loginClient.spec.js
@@ -1,9 +1,15 @@
 /// <reference types="cypress" />
 /* eslint-disable no-undef */
 
+const localUrl = 'http://localhost:3000/';
+// const quickCallUrl = 'https://quickcall.netlify.app/';
+
 describe('Login Client', () => {
   beforeEach(() => {
-    cy.visit('/');
+    cy.visit(localUrl);
   });
-  it('Login Client', () => {});
+
+  it('Login Client', () => {
+    cy.contains('Iniciar Sesión').click();
+  });
 });
