16f260446f91f6013def6cfb7d7a3696654d460f
Luis Salcedo Salas <rideluis@gmail.com>
2022-05-09 16:51:42 -0500

    feat: installed cypress

---
+++ cypress/integration/loginClient.spec.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+/* eslint-disable no-undef */
+
+describe('Login Client', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+  it('Login Client', () => {});
+});
